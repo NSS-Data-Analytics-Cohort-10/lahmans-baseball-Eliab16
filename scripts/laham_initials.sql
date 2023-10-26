a884ba18e1cefcc004d9d92ee0b9c67e01f35298
SELAMAWITMEKONNEN <kidestanchinesh@gmail.com>
2023-10-26 19:55:23 -0400

    q6

--- scripts/laham_initials.sql
+++ scripts/laham_initials.sql
@@ -21,51 +21,193 @@
 How many games did he play in? What is the name of the team for which he played?
  --ans
  
---  SELECT namefirst,height
-    FROM people
+ select*from appearances;
+ 
+  SELECT namefirst,namelast,height
+    FROM people as p
 	WHERE height IS NOT NULL
 	order by height ASC
 	LIMIT 1;
-
- select* from people;  
+ ---Eddie Gaedel ---height "43"
   
+    SELECT namefirst,height,name,
+	sum(G_all) as total_game
+    FROM people
+	INNER JOIN appearances
+	USING(playerid)
+	INNER JOIN teams
+	USING (teamid)
+	WHERE height IS NOT NULL 
+	GROUP BY namefirst,height,G_all,name
+	order by height ASC
+	LIMIT 1;
    
-
----3. Find all players in the database who played at Vanderbilt University. 
+ -- 52 games,St.Louis Browns
+  
+--3. Find all players in the database who played at Vanderbilt University. 
 -- ans
  
- SELECT schoolname,playerid,namefirst,namelast,namegiven
+ SELECT namefirst,namelast,namegiven,schoolname
  FROM schools
  JOIN collegeplaying
  USING(schoolid)
- JOIN 
- 
- 
- 
- 
- 
+ JOIN people
+ USING(playerid)
+ WHERE schoolname='Vanderbilt University'
+GROUP BY namefirst,namelast,namegiven,schoolname;	
 
-Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
-Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+---got 24 rows
+
+
+--- Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues.
+ 
+   
+	 SELECT namefirst,namelast,schoolname,
+	 sum(salary):: numeric:: money as total_salary
+	 FROM schools
+	INNER JOIN collegeplaying
+	USING(schoolid)
+	INNER JOIN Salaries
+	USING (playerid)
+	INNER JOIN people
+	USING (playerid)
+	where schoolname='Vanderbilt University'
+	group by namefirst,namegiven,namelast,schoolname
+	order by total_salary;
+-Sort this list in descending order by the total salary earned. 
+---Which Vanderbilt player earned the most money in the majors?
+
+  SELECT namefirst,namelast,schoolname,
+	max(salary):: numeric:: money as max_salary
+	 FROM schools
+	INNER JOIN collegeplaying
+	USING(schoolid)
+	INNER JOIN Salaries
+	USING (playerid)
+	INNER JOIN people
+	USING (playerid)
+	where schoolname='Vanderbilt University'
+	group by namefirst,namegiven,namelast,schoolname,salary;
+	
+  
+  
+    
+
+-- 4. Using the fielding table, 
+-- group players into three groups based on their position: 
+-- label players with position OF as "Outfield", 
+-- those with position "SS", "1B", "2B",and "3B" as "Infield", 
+-- and those with position "P" or "C" as "Battery". 
+-- Determine the number of putouts made by each of these three groups in 2016.
+
+ --ans          
+		 
+		  SELECT yearid, 
+		  SUM(po) as total_po,
+           CASE
+	    	WHEN pos IN ('SS','1B','2B','3B') THEN 'Infield'
+            WHEN pos IN ('P','C') THEN 'battery'
+            ELSE 'Outfield'
+		   
+		   
+-- ans total 7rows
+select*from pitching;
+		   
+-- 5. Find the average number of strikeouts per game by decade since 1920.
+Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+--ans   
+  SELECT yearid, (yearid/10)*10 AS decade,
+   ROUND (AVG(SO/G),2) AS AVG_strikeouts_per_game
+   FROM pitching
+   where yearid >=1920
+   GROUP BY yearid
+   ORDER BY AVG_strikeouts_per_game;
+   
+   
+ --ans
+   
+   SELECT yearid, (yearid/10)*10 AS decade,
+   ROUND (AVG(HR/G),2) AS AVG_Homeruns_per_game
+   FROM pitching
+   where yearid >=1920
+   GROUP BY yearid
+   ORDER BY AVG_Homeruns_per_game;
+ 
+  the year is after 1965 and Homeruns have lower avg_per_game
+ 
+   
+
+-- 6. Find the player who had the most success stealing bases in 2016,
+where __success__ is measured as the percentage of stolen base attempts which are successful
+. (A stolen base attempt results either in a stolen base or being caught stealing.) 
+Consider only players who attempted _at least_ 20 stolen bases.
 	
 
-4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
-   
-5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
-   
+7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series?
+What is the smallest number of wins for a team that did win the world series? 
+Doing this will probably result in an unusually small number of wins for a world series champion – 
+determine why this is the case. Then redo your query, excluding the problem year.
+How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? 
+What percentage of the time?
+--- 1981 small number of wins 
+---
+SELECT name,YearID,WSWin,SUM(W) as wins ,SUM(L) as losses
+FROM Teams
+WHERE yearID >=1970 AND  yearID !='1981'AND WSWin='Y' 
+GROUP BY name,YearID,WSwin
+ORDER BY wins DESC;
 
-6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
+--
+
+SELECT name,YearID,WSWin,SUM(W) as wins ,SUM(L) as losses
+FROM Teams
+WHERE yearID >=1970 AND  yearID !='1981'AND WSWin='N' 
+GROUP BY name,YearID,WSwin
+ORDER BY wins ASC;
+   
+--8. Using the attendance figures from the homegames table, 
+find the teams and parks which had the top 5 average attendance per game in 2016 
+(where average attendance is defined as total attendance divided by number of games). 
+Only consider parks where there were at least 10 games played. Report the park name, 
+team name, and average attendance. Repeat for the lowest 5 average attendance.
+
+
+  SELECT park_name,team,year,
+  sum(attendance)/sum(games) AS average_attendance
+  FROM homegames
+  INNER JOIN parks
+  using(park)
+  WHERE year='2016'  AND games>=10
+  GROUP BY team,year,park_name
+  ORDER BY average_attendance DESC
+  LIMIT 5;
+--   
+  SELECT park_name,team,year,
+  sum(attendance)/sum(games) AS average_attendance
+  FROM homegames
+  INNER JOIN parks
+  using(park)
+  WHERE year='2016'  AND games>=10
+  GROUP BY team,year,park_name
+  ORDER BY average_attendance ASC
+  LIMIT 5;
+ 
+
+
+9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)?
+Give their full name and 
+the teams that they were managing when they won the award.
+
+
 	
 
-7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+10. Find all players who hit their career highest number of home runs in 2016.
+Consider only players who have played in the league for at least 10 years, 
+and who hit at least one home run in 2016. 
+Report the players' first and last names and the number of home runs they hit in 2016.
 
 
-8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
-
-
-9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
-
-10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
+select*from pitching;
 
 
 **Open-ended questions**
